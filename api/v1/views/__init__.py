69d71c4ffb06b2daf8d916000e348e3bb3428e51
Santiago23z <santizapataalvares@gmail.com>
2022-09-16 13:11:48 -0500

    taks

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
 
-from flask import Blueprint, render_template, abort, request, jsonify
-from api.v1.views import api.v1.views.index
-import json
-import os
-import models
-from models import storage
-
 from flask import Blueprint
-from api.v1.views.index import *
 
 
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+app_views = Blueprint('/api/v1', __name__)
+from api.v1.views import index, states, cities, amenities, users, place_reviews, places
