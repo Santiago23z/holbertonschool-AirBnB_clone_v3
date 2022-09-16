7a72fd4cecec5964c61fc0757601b07283a8ebd0
Viktor <viktorkrilldj@gmail.com>
2022-09-16 11:52:41 -0500

    edit app

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,18 +1,27 @@
 #!/usr/bin/python3
 
-import storage from models
-import app_views from api.v1.views
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+from os import getenv
+from flask_cors import CORS
 
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
+CORS(app, origins=['0.0.0.0'])
+app.url_map.strict_slashes = False
+
+
+app.register_blueprint(app_views, url_prefix='/api/v1')
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """Remove the current SQLAlchemy"""
+def close_session(exception):
+    """Closes the session"""
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host='HBNB_API_HOST', port='5000', threaded=True)
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = getenv('HBNB_API_PORT', 5000)
+    app.run(host=host, port=port, threaded=True, debug=True)
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,5 +2,10 @@
 
 from flask import Blueprint, render_template, abort, request, jsonify
 from api.v1.views import api.v1.views.index
+import json
+import os
+import models
+from models import storage
+
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
