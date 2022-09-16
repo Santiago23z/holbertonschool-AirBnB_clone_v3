ec4b88807b5b3b1b2afa3c7598f29785552c9e1b
Viktor <viktorkrilldj@gmail.com>
2022-09-16 13:24:35 -0500

    edit app

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,13 +4,9 @@ from flask import Flask
 from models import storage
 from api.v1.views import app_views
 from os import getenv
-from flask_cors import CORS
 
 
 app = Flask(__name__)
-CORS(app, origins=['0.0.0.0'])
-app.url_map.strict_slashes = False
-
 
 app.register_blueprint(app_views, url_prefix='/api/v1')
 
