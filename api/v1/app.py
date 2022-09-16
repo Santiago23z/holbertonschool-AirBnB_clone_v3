ad5971e55eda90f364fd835e78d60993364d7139
Santiago23z <santizapataalvares@gmail.com>
2022-09-16 10:27:13 -0500

    taks

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
-import storage from models
-import app_views from api.v1.views
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
 
 
 app = Flask(__name__)
